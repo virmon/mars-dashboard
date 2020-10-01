4d69dc8aeadd59e1c9242f4dac08207422626029
virmon <virmontumulak@gmail.com>
2020-09-30 21:21:16 -0500

    Add components

--- src/public/client.js
+++ src/public/client.js
@@ -19,28 +19,7 @@ const render = async (root, state) => {
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
+    return Home(state)
 }
 
 // listening for load event because page should load before any JS is called
@@ -73,46 +52,110 @@ const ImageOfTheDay = (apod) => {
     console.log(photodate.getDate() === today.getDate());
     if (!apod || apod.date === today.getDate() ) {
         getImageOfTheDay(store)
+        getRover(`curiosity`)
+    } else {
+        return showError(apod)
     }
 
     // check if the photo of the day is actually type video!
-    if (apod.image.media_type === "video") {
+    if (apod.media_type === "video") {
         return (`
-            <p>See today's featured video <a href="${apod.image.url}">here</a></p>
-            <p>${apod.image.title}</p>
-            <p>${apod.image.explanation}</p>
+            <p>See today's featured video <a href="${apod.url}">here</a></p>
+            <p>${apod.title}</p>
+            <p>${apod.explanation}</p>
         `)
     } else {
         return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
+            <img src="${apod.url}" height="350px" width="100%" />
+            <p>${apod.explanation}</p>
         `)
     }
+}
+
+const Home = (state) => {
+    let { rovers, apod } = state
+
+    return `
+        <header></header>
+        <main>
+            ${Greeting(store.user.name)}
+            ${Menu(rovers)}
+            <section>
+                ${ImageOfTheDay(apod)}
+                ${PhotoList(store.activeMenu)}
+            </section>
+        </main>
+        <footer></footer>
+    `
+}
+
+const Menu = () => {
+    const { rovers } = store
+
+    return(`
+        <ul class='menu'>
+            <li class='menu-item'><a href='#'>APOD</a></li>
+            ${rovers.map((rover) => (
+                `<li id='${rover}' class='menu-item'><a href='/${rover}'>${rover}</a></li>`
+            )).join("")}
+        </ul>
+    `)
+}
+
+const PhotoList = (name) => {
+    const roverPhotos = store.roversPhoto[name].photos
+
+    return (`
+        <div class='gallery-title'>
+            <h3 class='rover-name'>${roverPhotos[0].rover.name}</h3>
+            <h4 class='latest-date'>${roverPhotos[0].earth_date}</h4>
+        </div>
+        <div class='gallery'>
+            ${roverPhotos.map(photo => (`
+                <ul class='photo-list'>
+                    <li class='photo-info'>ID: ${photo.id}</li>
+                    <li class='photo-info'>
+                        <img src='${photo.img_src}' alt='${photo.name}' />
+                    </li>
+                    <li class='photo-info'>Launch Date: ${photo.rover.launch_date}</li>
+                    <li class='photo-info'>Landing Date: ${photo.rover.landing_date}</li>
+                    <li class='photo-info'>Status: ${photo.rover.status}</li>
+                </ul>
+        `)).join("")}
+        </div>
+    `)
+}
+
+const showError = (err) => {
+    return(`
+        <h2>${err.code}: ${err.msg}</h2>
+    `)
 }
 
 // ------------------------------------------------------  API CALLS
 
 // Example API call
 const getImageOfTheDay = (state) => {
-    let { apod } = state
-
     fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
-
+        .then(apod => updateStore(store, { apod: apod.image }))
+        .catch(err => console.log(err))
     // return data
 }
 
 const getRover = rover => {
-    fetch(`http://localhost:3000/rovers/${rover}`)
+    fetch(`http://localhost:3000/rovers/${rover}/photos`)
         .then(res => res.json())
-        .then(images => 
+        .then(data => 
             updateStore(store, {
-                roversData: {
+                roversPhoto: {
                     [rover]: {
-                        photos: images.photos,
-                        info: images.photos[0].rover
+                        photos: data.latest_photos
                     }
-                }
+                },
+                activeMenu: rover
             }))
+            // updateStore(store, data))
+        // .catch(err => console.log(err))
+        .then(data => console.log(store))
 }
